64dfc6d795deeb922e9041fa53e0946f07708ea1
furion <_@furion.me>
2017-11-16 15:58:57 +0100

    dependency workaround

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = path.abspath(path.dirname(__file__))
 
 assert sys.version_info[0] == 3, "steemdata requires Python > 3"
 
-VERSION = '0.2.2'
+VERSION = '2.3'
 
 setup(
     name='steemdata',
@@ -25,44 +25,20 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
     ],
-    keywords='steem steemit',
+    keywords='steem',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     install_requires=[
-        'steem',
         'pymongo',
         'python-dateutil',
         'requests',
         'funcy',
         'werkzeug',
     ],
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
+    dependency_links=[
+        'git+git://github.com/Netherdrake/steem-python'
+    ],
     extras_require={
         'dev': ['check-manifest'],
         'test': ['coverage'],
     },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    # package_data={
-    #     'sample': ['package_data.dat'],
-    # },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    # entry_points={
-    #     'console_scripts': [
-    #         'sample=sample:main',
-    #     ],
-    # },
 )
